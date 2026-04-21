d4a383cf126aed1f37d8d551a1657b1fefaf2054
Artem Samsonov <samsonov.art@gmail.com>
2026-04-21 16:54:59 +0300

    Final stage

--- game.py
+++ game.py
@@ -49,6 +49,14 @@ def main():
 
         game.make_move(row, column, current_player)
         game.display()
+        # После каждого хода надо делать проверку на победу и на ничью.
+        if game.check_win(current_player):
+            print(f'Победили {current_player}!')
+            running = False
+        elif game.is_board_full():
+            print('Ничья!')
+            running = False
+            
         current_player = 'O' if current_player == 'X' else 'X'
 
 
